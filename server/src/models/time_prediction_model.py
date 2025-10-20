5071e709714488084d40f37df9d2113a450a0d8d
PXR <pxr2709@gmail.com>
2025-10-20 09:45:36 +0700

    cleanup

--- server/src/models/time_prediction_model.py
+++ server/src/models/time_prediction_model.py
@@ -2,9 +2,6 @@
 
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-import numpy as np
 import joblib
 import os
 
